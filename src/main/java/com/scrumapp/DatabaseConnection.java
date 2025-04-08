9097f0944d49570fe0f1db8e202f4d1f2b4c1c1e
Victor Hugo Soto <victorhsoto07@gmail.com>
2025-04-08 13:10:32 -0500

    Corrección sintaxis bd

--- src/main/java/com/scrumapp/DatabaseConnection.java
+++ src/main/java/com/scrumapp/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost.com:5432/educacion";
+    private static final String URL = "jdbc:postgresql://localhost:5432/educacion";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
 
