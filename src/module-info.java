e5c7bfcbd1dda3dfe32048002f86885d8d48f920
Victor Hugo Soto <victorhsoto07@gmail.com>
2025-05-17 16:03:25 -0500

    Corrección de las librerías de javafx

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,8 @@
-module ScrumApp {
+module com.scrumapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    
+
     opens com.scrumapp to javafx.fxml;
     exports com.scrumapp;
 }
